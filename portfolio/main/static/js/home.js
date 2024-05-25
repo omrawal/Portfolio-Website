9aaf4a48e34db0a873454c52f793cfad15e8cfb6
Om Rawal <omrawal007@gmail.com>
2024-05-25 14:08:40 +0530

    added frontend files

---
+++ portfolio/main/static/js/home.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function () {
+    const nameSearch = document.getElementById("name-search")
+    const tags = document.querySelectorAll('.tag')
+    const projects = document.querySelectorAll('.projects')
+
+    
+    
+})
